4f8157452e42f86e995a80c795b603954f0b4fe6
uluru-kinoshitat <t_kinoshita@uluru.jp>
2019-12-20 16:25:47 +0900

    add task execution policy

--- ecs_cluster.tf
+++ ecs_cluster.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_cluster" "njss3_ops" {
 }
 
 resource "aws_iam_role" "task_definitions_execution" {
-  name = "${var.service_name}${var.short_env}-ops-execution"
+  name = "${var.service_name}-${var.short_env}-ops-execution-role"
 
   assume_role_policy = <<EOF
 {
@@ -20,4 +20,28 @@ resource "aws_iam_role" "task_definitions_execution" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_policy" "task_definitions_execution" {
+  name = "${var.service_name}-${var.short_env}-ops-execution-policy"
+  description = "${var.service_name} ${var.short_env}  ops shell policy on ECS Fargate"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "events:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "task_definitions_execution" {
+  role       = aws_iam_role.task_definitions_execution.name
+  policy_arn = aws_iam_policy.task_definitions_execution.arn
 }
